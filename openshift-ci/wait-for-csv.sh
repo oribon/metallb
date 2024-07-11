36bd1a7be598c6ae80823a851dca6b5fc8611a3e
karampok <karampok@gmail.com>
2024-07-11 16:40:58 +0200

    OCP-CI: refactor wait-for-csv script
    Signed-off-by: karampok <karampok@gmail.com>

--- openshift-ci/wait-for-csv.sh
+++ openshift-ci/wait-for-csv.sh
@@ -1,48 +1,9 @@
 #!/bin/bash
 set -euo pipefail
 
-VERSION="v0.0.0"
-CSV_NAME="metallb-operator.${VERSION}"
-NAMESPACE="metallb-system"
+export VERSION="v0.0.0"
+export CSV_NAME="metallb-operator.${VERSION}"
+export NAMESPACE="metallb-system"
 
-ATTEMPTS=0
-MAX_ATTEMPTS=60
-csv_created=false
-until $csv_created || [ $ATTEMPTS -eq $MAX_ATTEMPTS ]
-do
-    echo "waiting for csv to be created attempt:${ATTEMPTS}"
-    if oc get csv -n $NAMESPACE $CSV_NAME; then
-        echo "csv created!"
-        csv_created=true
-    else    
-        echo "failed, retrying"
-        sleep 5
-    fi
-    (( ATTEMPTS++ ))
-done
-
-if ! $csv_created; then 
-    echo "Timed out waiting for csv to be created"
-    exit 1
-fi
-
-ATTEMPTS=0
-MAX_ATTEMPTS=60
-csv_succeeded=false
-until $csv_succeeded || [ $ATTEMPTS -eq $MAX_ATTEMPTS ]
-do
-    echo "waiting for csv to be in phase Succeeded attempt:${ATTEMPTS}"
-    if [[ $(oc get csv -n $NAMESPACE $CSV_NAME -o 'jsonpath={.status.phase}') == "Succeeded" ]]; then
-        echo "csv phase Succeeded"
-        csv_succeeded=true
-    else    
-        echo "failed, retrying"
-        sleep 5
-    fi
-    (( ATTEMPTS++ ))
-done
-
-if ! $csv_succeeded; then 
-    echo "Timed out waiting for csv to be in Succeeded phase"
-    exit 1
-fi
+timeout 5m bash -c 'until oc get csv -n $NAMESPACE $CSV_NAME; do sleep 5; done'
+oc wait --for jsonpath='{.status.phase}'=Succeeded csv/"$CSV_NAME" -n "$NAMESPACE" --timeout=300s
